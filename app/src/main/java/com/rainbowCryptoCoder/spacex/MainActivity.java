daf7ed2b062f5f2488284414a946640d0804d2f5
2019cstushar7363@poornima.edu.in <GOODLUCK@4me>
2021-07-10 15:45:04 +0530

    Initial commit

---
+++ app/src/main/java/com/rainbowCryptoCoder/spacex/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rainbowCryptoCoder.spacex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
